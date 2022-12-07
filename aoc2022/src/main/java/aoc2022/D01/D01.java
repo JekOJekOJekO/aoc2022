6b4b2095e8b7877bfb57825fa4b6e4f8fd400993
JekOJekOJekO <jens.kostka@googlemail.com>
2022-12-07 21:01:05 +0000

    Day 01

---
+++ aoc2022/src/main/java/aoc2022/D01/D01.java
@@ -0,0 +1,65 @@
+package aoc2022.D01;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import aoc2022.AocBase;
+
+public class D01 extends AocBase {
+	
+	
+	public D01() {
+		super("/D01-Input.txt");
+	}
+
+	public void doit() throws IOException {
+		
+		List<Integer> list = new LinkedList<>();
+	
+		String line = reader.readLine();
+
+		int tmp = 0;
+		while(true) {
+			
+			if (line == null) {
+				break;
+			}
+			
+			if (line.strip().isEmpty() == false) {
+				tmp += Integer.parseInt(line);
+
+			} else {
+				list.add(tmp);
+				tmp = 0;
+			}
+			line = reader.readLine();
+		
+		}
+
+		reader.close();
+		
+		int size = list.size(), max = 0;
+		
+		for(int i = 0 ; i < size ; i++) {
+			int current = list.get(i);
+			if(current > max) {
+				max = current;
+			}
+		}
+		// Aufgabe 1
+		System.out.println("Aufgabe 1:"); // 72070
+		System.out.println(max);
+		System.out.println(" ");
+		
+		// Aufgabe 2
+		Collections.sort(list);
+		System.out.println("Aufgabe 2:"); // 211805
+		int sum = list.get(size - 1);
+		sum += list.get(size - 2);
+		sum += list.get(size - 3);
+		System.out.println(sum);
+			
+	}
+}
